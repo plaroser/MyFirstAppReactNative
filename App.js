ade48a3c247cdeb32ce807512f66e71df2699598
MSI-SOBREMESA <git@bitbucket.org:plaroser/pruebaandroid.git>
2020-02-26 19:10:03 +0100

    Going back

--- App.js
+++ App.js
@@ -22,11 +22,14 @@ function DetailsScreen({ navigation }) {
       <Text>Details Screen</Text>
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => navigation.push('Details')}
       />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
+
 const Stack = createStackNavigator();
 
 function App() {
